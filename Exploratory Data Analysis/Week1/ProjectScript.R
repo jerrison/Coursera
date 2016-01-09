62a7991985d7fde8f0cbd6091bab7a09230c8b9e
Jerrison Li <jll09c@my.fsu.edu>
2016-01-08 17:45:10 -0800

    started working on project

---
+++ Exploratory Data Analysis/Week1/ProjectScript.R
@@ -0,0 +1,12 @@
+###############################################################################
+# Author: Jerrison Li
+# Date: 01.08.2016 5:24:35 PM 
+# Description: 
+#
+###############################################################################
+
+setwd("H:/Documents/GitHub/Coursera/Exploratory Data Analysis/Week1")
+
+library(downloader)
+download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+         destfile = "HHPC.zip")
