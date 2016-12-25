16b0cebf6b627ee283065db1657e6cdcb09f70b5
Jerrison Li <jll09c@my.fsu.edu>
2016-12-24 20:30:46 -0800

    Updating everything

--- Reproducible_Research/Week1/chapter_7_script.R
+++ Reproducible_Research/Week1/chapter_7_script.R
@@ -56,3 +56,91 @@ plot(hClusterUpdated)
 # whether an email is spam or not.  We will cycle through all the predictors and
 # subsquently calculate the cross validated error rate of predicting spam emails
 # from a single variable
+
+
+trainSpam$numType <- as.numeric(trainSpam$type) - 1
+costFunction <- function(x, y) sum(x != (y > 0.5))
+cvError <- rep(NA, 55)
+library(boot)
+
+for (i in 1:55) {
+    lmFormula <- reformulate(names(trainSpam)[i], response = "numType")
+    glmFit = glm(lmFormula, family = "binomial", data = trainSpam)
+    cvError[i] = cv.glm(trainSpam, glmFit, costFunction, 2)$delta[2]
+}
+
+## which predictor has minimum cross-validated error?
+names(trainSpam)[which.min(cvError)]
+
+## Use the best model from the group
+predictionModel <- glm(numType ~ charDollar, family = "binomial", data = 
+                           trainSpam)
+
+
+## Get predictions on the test set
+predictionTest = predict(predictionModel, testSpam)
+predictedSpam <- rep("nonspam", dim(testSpam)[1])
+
+## Classify as 'spam' for those with prob > 0.5
+predictedSpam[predictionModel$fitted > 0.5] <- "spam"
+
+
+table(predictedSpam, testSpam$type)
+
+## Error rate
+
+diag(table(predictedSpam, testSpam$type))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
